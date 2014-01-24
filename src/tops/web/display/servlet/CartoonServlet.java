b47f366da7bfa798ce14f6dca356641e46c3b6e1
maclean <gilleain.torrance@gmail.com>
2014-01-24 00:22:20 +0000

    Some refactoring of the cartoon servlet

--- src/tops/web/display/servlet/CartoonServlet.java
+++ src/tops/web/display/servlet/CartoonServlet.java
@@ -1,12 +1,21 @@
 package tops.web.display.servlet;
 
-import java.io.*;
-import java.util.*;
-
-import javax.servlet.*;
-import javax.servlet.http.*;
-
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import tops.dw.protein.CATHcode;
 import tops.dw.protein.Protein;
@@ -29,95 +38,32 @@ public class CartoonServlet extends HttpServlet {
     /*
      * horrible hack to ensure images have correct filenames. uses extra path
      * info to the servlet as the parameters so a request for
-     * '/tops/path-servlet-name/group/chaindomain.ext' gets chain.tops (or db
-     * equiv.) from whatever directory 'group' maps to and renders it as a
-     * ".ext" file. 'nice'
+     * '/tops/path-servlet-name/group/chaindomain.ext' gets chain.tops (or db equiv.) 
+     * from whatever directory 'group' maps to and renders it as an ".ext" file. 'nice'
      */
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String path = request.getPathInfo(); // eg /cath/1.2/2bopA0.gif OR
-                                                // /cath/2bopA0.gif
-        if (path == null) {
-            this.error("Directory not found", response);
-            return;
-        }
-
-        path = path.substring(0); // crude way to chomp off the first
-                                    // character
-        String[] bits = path.split("/"); // get the bits
-
-        if (bits.length == 0) {
-            this.error("No file specified", response);
-            return;
-        }
-
-        String group, domain, chain, filename, fileType;
-        int width = 0;
-        int height = 0;
-        String highlight = null;
-        String size = null;
-        try {
-            group = bits[1]; // eg 'cath'
-
-            String file = null;
-            if (bits.length == 5) { // size, highlight AND name
-                size = bits[2];
-                highlight = bits[3];
-                file = bits[4];
-            } else if (bits.length == 4) {
-                if (bits[2].indexOf('x') != -1) { // test to see if the string
-                                                    // has an 'x' in it!
-                    size = bits[2]; // eg '200x200'
-                } else {
-                    highlight = bits[2]; // eg '1.2.5'
-                }
-
-                file = bits[3]; // eg '2bopA0.gif'
-            } else {
-                file = bits[2];
-            }
-
-            if (size != null) {
-                int xPos = size.indexOf('x');
-                String wStr = size.substring(0, xPos);
-                width = Integer.parseInt(wStr);
-                String hStr = size.substring(xPos + 1);
-                height = Integer.parseInt(hStr);
-            }
-            if (width == 0)
-                width = CartoonServlet.DEFAULT_WIDTH;
-            if (height == 0)
-                height = CartoonServlet.DEFAULT_HEIGHT;
-
-            int dot = file.indexOf("."); // get start of filetype
-            fileType = file.substring(dot + 1); // get extension
-
-            domain = file.substring(0, dot); // eg '2bopA0'
-            chain = domain.substring(0, 5); // eg '2bopA'
-            filename = chain + ".tops"; // fixed, but hey
-
-        } catch (StringIndexOutOfBoundsException sioobe) {
-            this.error("Filename incorrect?", response);
-            return;
-        }
-
+        String path = request.getPathInfo(); // eg /cath/1.2/2bopA0.gif OR /cath/2bopA0.gif
+        Map<String, String> params = parsePath(path, response);
+        
         ServletConfig config = this.getServletConfig();
-        String pathToFiles = config.getInitParameter(group);
-        this.log("path = " + pathToFiles + " group = " + group + " fileType = "
-                + fileType + " domain = " + domain);
+        String pathToFiles = config.getInitParameter(params.get("group"));
+        this.log("path = " + pathToFiles 
+        		+ " group = " + params.get("group") 
+        		+ " fileType = " + params.get("fileType") 
+        		+ " domain = " + params.get("domain") );
 
         // first, try and get the data from the given source
-        File f = new File(pathToFiles, filename);
+        File f = new File(pathToFiles, params.get("filename"));
         if (!f.canRead()) {
-            this.error("File not found " + filename, response);
+            this.error("File not found " + params.get("filename"), response);
             return;
         }
 
         // quickly check if we only want the text of the file
-        if (fileType.equals("tops")) { // TOPS! file format (basically,
-                                        // old-style tops file!
+        if (params.get("fileType").equals("tops")) { // TOPS! file format (basically, old-style tops file!)
             response.setContentType("text/plain");
             PrintWriter out = response.getWriter();
             String line;
@@ -135,21 +81,25 @@ public class CartoonServlet extends HttpServlet {
             return;
         }
 
-        // second, assuming the tops.dw.protein can be created, determine what
-        // to return
-        SecStrucElement root = this.getRoot(domain, f);
+        // assuming the tops.dw.protein can be created, determine what to return
+        SecStrucElement root = this.getRoot(params.get("domain"), f);
+        
         // do highlights
+        String highlight = params.get("highlight");
         if (highlight != null) {
             this.highlight(root, highlight);
         }
 
+        String domain = params.get("domain");
         if (root == null) {
             this.error("Domain not found : " + domain, response);
             return;
         }
 
+        String fileType = params.get("fileType");
+        int width = Integer.parseInt(params.get("width"));
+        int height = Integer.parseInt(params.get("height"));
         CartoonDrawer drawer = new CartoonDrawer();
-
         if (fileType.equals("gif")) { // Image - could be several types?
 
             response.setContentType("image/gif");
@@ -157,16 +107,14 @@ public class CartoonServlet extends HttpServlet {
 
             drawer.draw(domain, "IMG", width, height, root, out);
 
-        } else if (fileType.equals("pdf")) { // Portable Document Format,
-                                                // using iText library
+        } else if (fileType.equals("pdf")) { // Portable Document Format, using iText library
 
             response.setContentType("application/pdf");
             OutputStream out = response.getOutputStream();
 
             drawer.draw(domain, "PDF", width, height, root, out);
 
-        } else if (fileType.equals("svg")) { // Support Vector Graphics -
-                                                // basically XML
+        } else if (fileType.equals("svg")) { // Support Vector Graphics - basically XML
 
             response.setContentType("image/svg+xml");
             PrintWriter writer = response.getWriter();
@@ -185,6 +133,80 @@ public class CartoonServlet extends HttpServlet {
             return;
         }
 
+    }
+    
+    private Map<String, String> parsePath(String path, HttpServletResponse response) throws IOException {
+    	Map<String, String> params = new HashMap<String, String>();
+    	if (path == null) {
+    		this.error("Directory not found", response);
+    		return null;
+    	}
+
+    	path = path.substring(0); // crude way to chomp off the first character
+    	String[] bits = path.split("/"); // get the bits
+
+    	if (bits.length == 0) {
+    		this.error("No file specified", response);
+    		return null;
+    	}
+
+    	int width = 0;
+    	int height = 0;
+    	String size = null;
+    	try {
+    		String group = bits[1]; // eg 'cath'
+    		params.put("group", group);
+
+    		String file = null;
+    		String highlight = null;
+    		if (bits.length == 5) { // size, highlight AND name
+    			size = bits[2];
+    			highlight = bits[3];
+    			file = bits[4];
+    		} else if (bits.length == 4) {
+    			if (bits[2].indexOf('x') != -1) { // test to see if the string has an 'x' in it!
+    				size = bits[2]; // eg '200x200'
+    			} else {
+    				highlight = bits[2]; // eg '1.2.5'
+    			}
+
+    			file = bits[3]; // eg '2bopA0.gif'
+    		} else {
+    			file = bits[2];
+    		}
+    		params.put("highlight", highlight);
+
+    		if (size != null) {
+    			int xPos = size.indexOf('x');
+    			String wStr = size.substring(0, xPos);
+    			params.put("width", wStr);
+    			String hStr = size.substring(xPos + 1);
+    			params.put("height", hStr);
+    		}
+    		if (width == 0)
+    			width = CartoonServlet.DEFAULT_WIDTH;
+    		if (height == 0)
+    			height = CartoonServlet.DEFAULT_HEIGHT;
+
+    		int dot = file.indexOf("."); // get start of filetype
+    		String fileType = file.substring(dot + 1); // get extension
+    		params.put("fileType", fileType);
+
+    		String domain = file.substring(0, dot); // eg '2bopA0'
+    		params.put("domain", domain);
+
+    		String chain = domain.substring(0, 5); // eg '2bopA'
+    		params.put("chain", chain);
+
+    		String filename = chain + ".tops"; // fixed, but hey
+    		params.put("filename", filename);
+
+    	} catch (StringIndexOutOfBoundsException sioobe) {
+    		this.error("Filename incorrect?", response);
+    		return null;
+    	}
+
+    	return params;
     }
 
     private void highlight(SecStrucElement root, String highlight) {
@@ -223,18 +245,15 @@ public class CartoonServlet extends HttpServlet {
     }
 
     private SecStrucElement getRoot(String domid, File f) throws IOException {
-
         Protein p = new Protein(f);
 
         Vector<SecStrucElement> doms = p.GetLinkedLists();
-        // Vector labs = p.GetDomainDefs();
         int domainIndex = p.GetDomainIndex(new CATHcode(domid));
         if (domainIndex == -1) {
             return null;
         }
 
-        return (SecStrucElement) doms.elementAt(domainIndex);
-        // String label = (labs.elementAt(domainIndex)).toString();
+        return doms.elementAt(domainIndex);
     }
 
     private void error(String problem, HttpServletResponse response) {
@@ -246,9 +265,8 @@ public class CartoonServlet extends HttpServlet {
         }
 
         pout.println("<html><head><title>ERROR</title></head>");
-        pout.println("<body>Sorry, could not draw this cartoon. Reason : <p>"
-                + problem);
+        pout.println("<body>Sorry, could not draw this cartoon. Reason : <p>" + problem);
         pout.println("</body></html>");
     }
 
-} // EOC
+}
