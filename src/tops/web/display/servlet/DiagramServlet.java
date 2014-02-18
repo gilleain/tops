c67fa7bb1bdca8b8234507efcb47751785eb0e0c
maclean <gilleain.torrance@gmail.com>
2014-02-18 08:09:53 +0000

    Clean up the DiagramServlet.

--- src/tops/web/display/servlet/DiagramServlet.java
+++ src/tops/web/display/servlet/DiagramServlet.java
@@ -15,21 +15,11 @@ import tops.view.tops2D.diagram.DiagramDrawer;
 
 public class DiagramServlet extends HttpServlet {
 
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 2431033580433443788L;
 
 	private int DEFAULT_WIDTH = 300;
 
     private int DEFAULT_HEIGHT = 200;
-
-    private int w, h;
-
-//    private String head;
-    private String body;
-    private String tail;
-    private String matches;
 
     static {
         System.setProperty("java.awt.headless", "true");
@@ -38,47 +28,47 @@ public class DiagramServlet extends HttpServlet {
     @Override
     public void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String path = request.getPathInfo(); // like
-                                                // "/200/300/NEEC/1:2P//2bopA0.gif
-        // String topsString = request.getParameter("data");
+        String path = request.getPathInfo(); // like "/200/300/NEEC/1:2P//2bopA0.gif
         path = path.substring(1); // chomp first char
         String[] bits = path.split("/");
 
         String width = bits[0];
-        // String width = request.getParameter("width");
         String height = bits[1];
-        // String height = request.getParameter("height");
 
-        // matches = request.getParameter("match");
-        this.matches = bits[2];
+        String matches = bits[2];
 
-        this.body = bits[3];
-        this.tail = bits[4];
-//        head = bits[5];
+        String body = bits[3];
+        String tail = bits[4];
 
-        if (this.tail.equals("none"))
-            this.tail = new String();
-        if (this.matches.equals("none"))
-            this.matches = new String();
+        if (tail.equals("none")) {
+            tail = new String();
+        }
+        if (matches.equals("none")) {
+            matches = new String();
+        }
 
-        if (width.equals("default"))
-            this.w = this.DEFAULT_WIDTH;
-        else
-            this.w = Integer.parseInt(width);
+        int w;
+        if (width.equals("default")) {
+            w = this.DEFAULT_WIDTH;
+        } else {
+            w = Integer.parseInt(width);
+        }
 
-        if (height.equals("default"))
-            this.h = this.DEFAULT_HEIGHT;
-        else
-            this.h = Integer.parseInt(height);
+        int h;
+        if (height.equals("default")) {
+            h = this.DEFAULT_HEIGHT;
+        } else {
+            h = Integer.parseInt(height);
+        }
 
         response.setContentType("image/gif");
         OutputStream out = response.getOutputStream();
 
         int type = BufferedImage.TYPE_INT_RGB; // which type?
-        BufferedImage image = new BufferedImage(this.w, this.h, type);
+        BufferedImage image = new BufferedImage(w, h, type);
         Graphics2D g2 = image.createGraphics();
 
-        DiagramDrawer dd = new DiagramDrawer(this.body, this.tail, this.matches, this.w, this.h);
+        DiagramDrawer dd = new DiagramDrawer(body, tail, matches, w, h);
         dd.paint(g2);
 
        ImageIO.write(image, "GIF", out);
