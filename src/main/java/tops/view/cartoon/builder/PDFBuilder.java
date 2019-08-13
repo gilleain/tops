922c6cc5020cb47cf15fcf2fef4660db9915090e
gilleain <gilleain.torrance@gmail.com>
2019-08-13 22:52:11 +0100

    Fix CVE-2019-0228 by upgrading pdfbox

--- src/main/java/tops/view/cartoon/builder/PDFBuilder.java
+++ src/main/java/tops/view/cartoon/builder/PDFBuilder.java
@@ -1,6 +1,6 @@
 package tops.view.cartoon.builder;
 
-//Make PDF files using the iText library
+//Make PDF files using pdfbox
 
 import java.awt.Color;
 import java.awt.Image;
@@ -9,37 +9,33 @@ import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.logging.Logger;
 
-import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.edit.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDJpeg;
-import org.apache.pdfbox.pdmodel.graphics.xobject.PDXObjectImage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 import tops.view.cartoon.ByteCartoonBuilder;
 
 public class PDFBuilder implements ByteCartoonBuilder {
 
+    private static Logger logger = Logger.getLogger(PDFBuilder.class.getName());
+    
     private GraphicsBuilder graphicsBuilder;
 
     private PDDocument document; // the product that this builder makes
     
     public PDFBuilder(Image image, Rectangle bb) {
-    	try {
-    		this.document = new PDDocument();
-
-    		PDPage page = new PDPage();
-    		
-    		graphicsBuilder = new GraphicsBuilder(image.getGraphics(), "", bb, bb.width, bb.height);
-
-    		PDXObjectImage ximage = new PDJpeg(document, (BufferedImage) image);
-    		PDPageContentStream content = new PDPageContentStream(document, page);
+        this.document = new PDDocument();
+        graphicsBuilder = new GraphicsBuilder(image.getGraphics(), "", bb, bb.width, bb.height);
+        
+    	try (PDPageContentStream content = new PDPageContentStream(document, new PDPage())) {
+    	    PDImageXObject ximage = LosslessFactory.createFromImage(document, (BufferedImage) image);
     		content.drawImage(ximage, 50, 400);
-    		content.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.warning(e.toString());
 		}
     }
 
@@ -48,11 +44,7 @@ public class PDFBuilder implements ByteCartoonBuilder {
         	this.document.save(output);
 			this.document.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (COSVisitorException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		    logger.warning(e.toString());
 		}
     }
 
